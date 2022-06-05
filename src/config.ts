1805673b6bba22e0209c53c9c771cf0e97b78152
Shavaun MacArthur <shavaunmac@gmail.com>
2022-06-06 10:58:53 +1200

    Joker rules for bonus yahtzee

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { YahtzeeScoreCategory } from "./types";
+
 export default {
   diceCount: 5,
   rollsPerTurn: 3,
@@ -5,8 +7,16 @@ export default {
     rollPrompt: "What do you want to do?",
     diceLockPrompt: "Which dice do you want to lock? (press space to toggle lock status)",
     scoreDicePrompt: "Which category do you want to score?",
+    scoreJokerPrompt: "Which category do you want to score a joker in?",
     scoresheetPrompt: "What do you want to do?",
     playAgainPrompt: "Do you want to play again?",
     quitConfirmPrompt: "Are you sure you want to quit?",
+  },
+  scoreValues: {
+    [YahtzeeScoreCategory.FullHouse]: 25,
+    [YahtzeeScoreCategory.SmallStraight]: 30,
+    [YahtzeeScoreCategory.LargeStraight]: 40,
+    [YahtzeeScoreCategory.Yahtzee]: 50,
+    [YahtzeeScoreCategory.BonusYahtzees]: 100,
   },
 };
--- src/handleScoreDiceMode.ts
+++ src/handleScoreDiceMode.ts
@@ -37,19 +37,23 @@ function calculateScore(category: YahtzeeScoreCategory, _diceRoll: number[]) {
     case YahtzeeScoreCategory.FullHouse:
       return (diceRoll.find(d => diceRoll.filter(dd => dd === d).length === 2)
         && diceRoll.find(d => diceRoll.filter(dd => dd === d).length === 3))
-        ? 25 : 0;
+        ? config.scoreValues[YahtzeeScoreCategory.FullHouse] : 0;
     case YahtzeeScoreCategory.SmallStraight:
       return ["12345", "23456", "1234", "2345", "3456", "13456", "12346"]
-        .includes(Array.from(new Set(diceRoll)).sort().join("")) ? 30 : 0;
+        .includes(Array.from(new Set(diceRoll)).sort().join(""))
+        ? config.scoreValues[YahtzeeScoreCategory.SmallStraight] : 0;
     case YahtzeeScoreCategory.LargeStraight:
       return ["12345", "23456"]
-        .includes(diceRoll.sort().join("")) ? 40 : 0;
+        .includes(diceRoll.sort().join(""))
+        ? config.scoreValues[YahtzeeScoreCategory.LargeStraight] : 0;
     case YahtzeeScoreCategory.Yahtzee:
-      return isYahtzee(diceRoll) ? 50 : 0;
+      return isYahtzee(diceRoll)
+        ? config.scoreValues[YahtzeeScoreCategory.Yahtzee] : 0;
     case YahtzeeScoreCategory.Chance:
       return sumDiceRoll(diceRoll);
     case YahtzeeScoreCategory.BonusYahtzees:
-      return isYahtzee(diceRoll) ? 100 : 0;
+      return isYahtzee(diceRoll)
+        ? config.scoreValues[YahtzeeScoreCategory.BonusYahtzees] : 0;
     default:
       return 0;
   }
@@ -59,41 +63,80 @@ function sumDiceRoll(diceRoll: number[]) {
   return diceRoll.reduce((acc, cur) => acc + cur, 0);
 }
 
-function getPromptChoices(game: IGame) {
+function getPromptChoices(game: IGame, joker?: boolean) {
   const choices = [];
 
-  Object.keys(game.score).forEach(key => {
-    const category = key as YahtzeeScoreCategory;
-
-    if (category === YahtzeeScoreCategory.BonusYahtzees) {
+  if (joker) {
+    [
+      YahtzeeScoreCategory.Ones,
+      YahtzeeScoreCategory.Twos,
+      YahtzeeScoreCategory.Threes,
+      YahtzeeScoreCategory.Fours,
+      YahtzeeScoreCategory.Fives,
+      YahtzeeScoreCategory.Sixes,
+      YahtzeeScoreCategory.ThreeOfAKind,
+      YahtzeeScoreCategory.FourOfAKind,
+      YahtzeeScoreCategory.FullHouse,
+      YahtzeeScoreCategory.SmallStraight,
+      YahtzeeScoreCategory.LargeStraight,
+      YahtzeeScoreCategory.Chance,
+    ].forEach(key => {
+      const category = key as YahtzeeScoreCategory;
+      if ([
+        YahtzeeScoreCategory.FullHouse,
+        YahtzeeScoreCategory.SmallStraight,
+        YahtzeeScoreCategory.LargeStraight,
+      ].includes(category)) {
+        choices.push({
+          message: scoreLabels[category],
+          name: category,
+          value: category,
+          hint: config.scoreValues[category],
+        });
+      } else {
+        choices.push({
+          message: scoreLabels[category],
+          name: category,
+          value: category,
+          hint: calculateScore(category, game.diceRoll),
+        });
+      }
+    });
+  } else {
+    Object.keys(game.score).forEach(key => {
+      const category = key as YahtzeeScoreCategory;
+  
+      if (category === YahtzeeScoreCategory.BonusYahtzees) {
+        const disabled = game.score[YahtzeeScoreCategory.Yahtzee] === null
+          || calculateScore(category, game.diceRoll) === 0;
+        choices.push({
+          message: scoreLabels[category],
+          name: category,
+          value: category,
+          hint: `[${game.score[category]}]${(!disabled &&
+            category === YahtzeeScoreCategory.BonusYahtzees) ? " + 100" : ""}`,
+          disabled,
+        });
+      } else {
+        choices.push({
+          message: scoreLabels[category],
+          name: category,
+          value: category,
+          hint: game.score[category] !== null
+            ? `[${game.score[category]}]`
+            : calculateScore(category, game.diceRoll),
+          disabled: game.score[category] !== null,
+        });
+      }
+    });
+  
+    if (config.rollsPerTurn - game.rollNumber > 0) {
       choices.push({
-        message: scoreLabels[category],
-        name: category,
-        value: category,
-        hint: `[${game.score[category]}]${
-          category === YahtzeeScoreCategory.BonusYahtzees ? " + 100" : ""}`,
-        disabled: game.score[YahtzeeScoreCategory.Yahtzee] === null
-          || calculateScore(category, game.diceRoll) === 0
-      });
-    } else {
-      choices.push({
-        message: scoreLabels[category],
-        name: category,
-        value: category,
-        hint: game.score[category] !== null
-          ? `[${game.score[category]}]`
-          : calculateScore(category, game.diceRoll),
-        disabled: game.score[category] !== null,
+        message: "Cancel",
+        name: "cancel",
+        value: "cancel",
       });
     }
-  });
-
-  if (config.rollsPerTurn - game.rollNumber > 0) {
-    choices.push({
-      message: "Cancel",
-      name: "cancel",
-      value: "cancel",
-    });
   }
 
   return choices;
@@ -104,7 +147,7 @@ export function resetDiceRoll(game: IGame) {
   game.diceRoll = game.diceRoll.map(() => 0);
 }
 
-export default async function handleScoreDiceMode(game: IGame): Promise<IGame> {
+export default async function handleScoreDiceMode(game: IGame, joker?: boolean): Promise<IGame> {
   const _game = { ...game };
 
   drawTurnStats(game);
@@ -112,7 +155,9 @@ export default async function handleScoreDiceMode(game: IGame): Promise<IGame> {
 
   const prompt = new Select({
     name: "scoreDiceMenu",
-    message: config.messages.scoreDicePrompt,
+    message: joker
+      ? config.messages.scoreJokerPrompt
+      : config.messages.scoreDicePrompt,
     choices: getPromptChoices(_game),
   });
 
@@ -120,16 +165,21 @@ export default async function handleScoreDiceMode(game: IGame): Promise<IGame> {
     if (answer === "cancel") {
       return changeMode(_game, GameMode.ROLL);
     }
+
     const category = answer as YahtzeeScoreCategory;
+
     if (category === YahtzeeScoreCategory.BonusYahtzees
       && _game.score[YahtzeeScoreCategory.Yahtzee] !== null) {
       _game.score[YahtzeeScoreCategory.BonusYahtzees] += calculateScore(category, _game.diceRoll);
+      return handleScoreDiceMode(_game, joker);
     } else {
       _game.score[category] = calculateScore(category, _game.diceRoll);
     }
+
     if (_game.turn === 12) {
       return changeMode(_game, GameMode.GAME_OVER);
     } 
+
     resetDiceLock(_game);
     _game.turn++;
     _game.rollNumber = 0;
