3e77bafe3ad03d7da8f623c06c93dc955c91e629
Shavaun MacArthur <shavaunmac@gmail.com>
2022-06-25 17:10:43 +1200

    Add missing util unit test

---
+++ src/utils/screen.test.ts
@@ -0,0 +1,12 @@
+import { constructChoice } from "./screen";
+
+describe("constructChoice", () => {
+  test("it constructs a choice", () => {
+    const choice = constructChoice("a", { a: "A" });
+    expect(choice).toEqual({
+      name: "a",
+      value: "a",
+      message: "A",
+    });
+  });
+});
