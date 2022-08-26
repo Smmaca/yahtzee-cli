6f7f71c1bea43bce9cdcfa84a928557d37322877
Shavaun MacArthur <shavaunmac@gmail.com>
2022-08-26 17:02:34 +1200

    Add e2e test to change dice design from main menu

--- e2e/tests/mainMenuOptions.test.ts
+++ e2e/tests/mainMenuOptions.test.ts
@@ -6,6 +6,8 @@ import testConfig from "../testConfig";
 import { StatisticsScreenInput } from "../../src/gameScreens/StatisticsScreen";
 import { NewGameScreenInput } from "../../src/gameScreens/NewGameScreen";
 import { Screen } from "../../src/types";
+import { DiceDesign } from "../../src/utils/diceDesigns";
+import { SettingsScreenInput } from "../../src/gameScreens/SettingsScreen";
 
 describe("Main menu options", () => {
   test("quit, cancel, then actually quit from main menu", async () => {
@@ -68,6 +70,32 @@ describe("Main menu options", () => {
       Screen.MAIN_MENU,
       Screen.QUIT_CONFIRM,
     ]);
+  });
+
+  test("go to settings and change dice design", async () => {
+    const prompter = new MockPrompter([
+      { promptName: Screen.MAIN_MENU, answer: MainMenuScreenInput.SETTINGS },
+      { promptName: Screen.SETTINGS, answer: SettingsScreenInput.DICE_DESIGN },
+      { promptName: Screen.DICE_DESIGNER, answer: DiceDesign.DIGITS },
+      { promptName: Screen.SETTINGS, answer: SettingsScreenInput.BACK },
+      { promptName: Screen.MAIN_MENU, answer: MainMenuScreenInput.QUIT },
+      { promptName: Screen.QUIT_CONFIRM, answer: true },
+    ]);
+
+    const dice = new MockDice();
+
+    const endGameState = await main(testConfig, prompter, dice);
+
+    expect(endGameState.screenHistory).toEqual([
+      Screen.MAIN_MENU,
+      Screen.SETTINGS,
+      Screen.DICE_DESIGNER,
+      Screen.SETTINGS,
+      Screen.MAIN_MENU,
+      Screen.QUIT_CONFIRM,
+    ]);
+
+    expect(endGameState.diceDesign).toBe(DiceDesign.DIGITS);
   });
 
   test("start a new game and then cancel", async () => {
